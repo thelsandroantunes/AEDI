31132eb78791d527882f80e42861d892bd68e0d9
Thelsandro <thelsandro.antunes@gmail.com>
2018-06-23 13:36:14 -0400

    Lista3
    Adicionado Busca Sequencial Recursiva

--- AEDI/Lista3_Recursividade.cpp
+++ AEDI/Lista3_Recursividade.cpp
@@ -1,8 +1,8 @@
 /*
     Programa: Recursividade
     Autor: Thelsandro Antunes
-    1ª Versão 
-    Concluída em: 22/06/2018
+    2ª Versão 
+    Concluída em: 23/06/2018
     Breve descrição:
         Este programa utiliza Funções em C++        
 */
@@ -11,6 +11,7 @@
 #include<ctime>
 #define VALIDO   1
 #define INVALIDO 0
+#define LIMITE 100
 
 using namespace std;
 
@@ -21,6 +22,9 @@ void printRecursive(int);
 void printRecursive2(int);
 void printRecursive3(int[], int);
 void aleatoriosNP(int[], int);
+int existeNumero(int[],int, int);
+void bubbleSort(int[],int);
+int buscaSequencialRecursiva(int, int[], int);
 void questao01();
 void questao02();
 void questao02(int);
@@ -87,8 +91,8 @@ void start()
 void menu()
 {
     cout<<" MENU "<<endl;
-    cout<<"1  - questão01:recursivo"<<endl;
-    cout<<"2  - questão02: recursivo inverso"<<endl;
+    cout<<"1  - questão01:recursivo(1,2,...,N)"<<endl;
+    cout<<"2  - questão02: recursivo inverso(N,...,2,1)"<<endl;
     cout<<"3  - questão03"<<endl;
     cout<<"4  - questão04"<<endl;
     cout<<"5  - questão05"<<endl;
@@ -127,36 +131,66 @@ void printRecursive2(int n)
 void printRecursive3(int v[], int tam)
 {
     if(tam == 1)
-        cout << v[tam] << " ";
+        cout << v[tam-1] << " ";
     else
     {
         printRecursive3(v, tam-1);
         cout << v[tam-1] << " ";
     } 
 }
-void aleatoriosNP(int v[], int tam)
+void aleatoriosNP(int v[], int tam)//negativos e positivos
 {   
     srand(time(NULL));
-    int flag;
-    for(int i = 0; i < tam; ++i) {
-        do 
+    int num;
+        
+    for(int i = 0; i < tam; i++)
+    {
+        num = rand() % LIMITE;
+        while(existeNumero(v,i, num) == 1)
         {
-            if(i%2==1)
+            num = rand() % LIMITE;
+        }
+        if(i%2==0) v[i] = num;
+        else v[i] = num*(-1);
+    }        
+}
+int existeNumero(int v[],int i, int num)
+{    
+    for(int j = 0; j < i; j++)
+    {
+        if(v[j]==num) 
+            return VALIDO;
+    }        
+    return INVALIDO;
+}
+void bubbleSort(int v[],int tam)
+{
+    int i,j;
+    int temp;
+    
+    for(int i = (tam-1); i >= 0; i--)
+    {   
+        for(int j = 1; j <= i; j++)
+        {              
+            if(v[j-1] > v[j])
             {
-                v[i] = (rand() % 1000) - 7;
-                flag = VALIDO;
+                temp = v[j-1];
+                v[j-1] = v[j];
+                v[j] = temp;
             }
-            else
-            {
-                v[i] = (rand() % 1000)*(-1) ;
-                flag = VALIDO;
-            }
-          
-            for(int j = 0; j < i; ++j)
-                if (v[i] == v[j])
-            flag = INVALIDO;
-       } while (flag == INVALIDO);   
-    }      
+        }
+    }    
+}
+int buscaSequencialRecursiva(int k, int v[], int tam)
+{   
+    if(tam == 0)
+        return (k == v[tam]) ? 1 : -1;
+    else
+    {
+        if(k == v[tam])
+            return 1;
+        buscaSequencialRecursiva(k,v,tam-1);        
+    }
 }
 void questao01()//OK
 {
@@ -180,10 +214,30 @@ void questao03()//OK
     int v[tam];
 
     aleatoriosNP(v,tam);
+    bubbleSort(v,tam);
     cout << endl;
     printRecursive3(v,tam);
 }
-void questao04(){}
+void questao04()
+{
+    int tam,k;
+
+    cout << "tamanho do vetor de inteiros >> ";
+    cin >> tam;
+    
+    int v[tam];    
+
+    aleatoriosNP(v,tam);
+    cout << endl;
+    bubbleSort(v,tam);
+    printRecursive3(v,tam);
+
+    cout << endl;
+    cout << "chave >> ";
+    cin >> k;
+
+    cout << "OK >>" <<buscaSequencialRecursiva(k,v,tam)<<endl;
+}
 void questao05(){}
 void questao06(){}
 void questao07(){}
