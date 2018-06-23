f5169344080dd7b51a705ec6420196f2e76a2da4
Thelsandro <thelsandro.antunes@gmail.com>
2018-06-23 17:36:15 -0400

    Lista 3
    Funçao Maior e Troca.

--- AEDI/Lista3_Recursividade.cpp
+++ AEDI/Lista3_Recursividade.cpp
@@ -1,7 +1,7 @@
 /*
     Programa: Recursividade
     Autor: Thelsandro Antunes
-    3ª Versão 
+    4ª Versão 
     Concluída em: 23/06/2018
     Breve descrição:
         Este programa utiliza Funções de Ordenação(Bubble Sort), Busca Sequencial e Binária em C++        
@@ -18,21 +18,26 @@ using namespace std;
 void start();
 void menu();
 int valorN();
+void copiaVetor(int[], int[], int);
 void printRecursive(int);
 void printRecursive2(int);
 void printRecursive3(int[], int);
 void aleatoriosNP(int[], int);
 int existeNumero(int[],int, int);
 void bubbleSort(int[],int);
+void bubbleSortRecursiva(int[],int);
 int buscaSequencialRecursiva(int, int[], int);
 int buscaBinariaRecursiva(int, int[], int, int);
 void questao01();
 void questao02();
-void questao02(int);
 void questao03();
 void questao04();
 void questao05();
 void questao06();
+int questao06A(int[], int);
+void questao06B(int v[], int, int);
+void questao06C(int[], int);
+void questao6AB(int[], int);
 void questao07();
 
 
@@ -96,8 +101,11 @@ void menu()
     cout<<"2  - questão02:recursivo inverso(N,...,2,1)"<<endl;
     cout<<"3  - questão03:recursivo v[] ordenado"<<endl;
     cout<<"4  - questão04:busca sequencial recursiva V[]"<<endl;
-    cout<<"5  - questão05"<<endl;
-    cout<<"6  - questão06"<<endl;
+    cout<<"5  - questão05:busca binaria recursiva v[]"<<endl;
+    cout<<"6  - questão06:"<<endl;
+    cout<<"     (A)obtém o máximo elemento do v[]"<<endl;
+    cout<<"     (B)troca máximo elemento com o último no v[]"<<endl;
+    cout<<"     (C)executa (A) e (B) nos subvetores v[n-2] e v[n-3]"<<endl;
     cout<<"7  - questão07"<<endl;
     cout<<"0 - SAIR"<<endl;
     cout<<endl;
@@ -109,6 +117,11 @@ int valorN()
     cout << "n >> ";
     cin >> n;
     return n;
+}
+void copiaVetor(int v1[], int v2[], int tam)
+{
+    for(int i = 0; i < tam; i++)
+        v1[i] = v2[i];
 }
 void printRecursive(int n)
 {
@@ -182,14 +195,32 @@ void bubbleSort(int v[],int tam)
         }
     }    
 }
+void bubbleSortRecursiva(int v[],int tam)
+{
+    if (tam == 1)
+        return;
+    
+    int temp;
+    for (int j=0; j<tam-1; j++)
+    {
+        if (v[j] > v[j+1])
+        {
+            temp = v[j+1];
+            v[j+1] = v[j];
+            v[j] = temp;
+        }
+    }
+ 
+    bubbleSortRecursiva(v, tam-1);
+}
 int buscaSequencialRecursiva(int k, int v[], int tam)
 {   
     if(tam == 0)
-        return (k == v[tam]) ? 1 : -1;
+        return (k == v[tam]) ? tam : -999;
     else
     {
         if(k == v[tam])
-            return 1;
+            return tam;
         buscaSequencialRecursiva(k,v,tam-1);        
     }
 }
@@ -207,7 +238,7 @@ int buscaBinariaRecursiva(int k, int v[], int i, int j)
             return buscaBinariaRecursiva(k, v, i, m-1);
     }
 
-    return -1;
+    return -999;
 }
 void questao01()//OK
 {
@@ -274,8 +305,83 @@ void questao05()//OK
     cout << "chave >> ";
     cin >> k;
 
-    if(buscaBinariaRecursiva(k,v,0,tam) == -1) cout << "NÃO ENCONTRADO" <<endl;
+    if(buscaBinariaRecursiva(k,v,0,tam) == -999) cout << "NÃO ENCONTRADO" <<endl;
     else cout << "Elemento encontrado " << endl;
 }
-void questao06(){}
+void questao06()
+{
+    int tam,k, max;
+    cout << "tamanho do vetor de inteiros >> ";
+    do{
+        cin >> tam;
+        cout<<"(tamanho > 4) >> ";
+    }while(tam < 5);
+    
+    int v[tam];    
+    
+    aleatoriosNP(v,tam);
+    cout << endl;
+    //bubbleSortRecursiva(v,tam);
+    questao6AB(v,tam);
+    cout<<endl;
+    cout << "Letra (C):" << endl;
+    questao06C(v,tam);
+}
+int questao06A(int v[], int tam)//Encontra o Maior elemento do Vetor NÃO ordenado
+{   
+    if (tam == 1) return (v[0]);
+    else {
+            int max;
+            max = questao06A(v, tam-1);
+
+            if (max > v[tam-1])
+                return max;
+            else 
+                return (v[tam-1]);
+    }
+}
+
+void questao06B(int v[], int tam, int max)//Troca o Maior elemento com o ÚLTIMO elemento do Vetor
+{
+    int ultimaPos = tam-1;
+    int aux,i;
+    i = buscaSequencialRecursiva(max,v,tam);
+    cout<<endl;
+    cout << "MAX >> [" <<max<<"] trocado pelo ULTIMO elemento do vetor >> ["<<v[ultimaPos]<<"] "<<endl;
+
+    aux = v[i];
+    v[i] = v[ultimaPos];
+    v[ultimaPos] = aux;
+
+}
+void questao06C(int v[], int tam)//Chama letra (A) e (B) para subVetores
+{
+    
+    int v1[tam-2];
+    int v2[tam-3];
+    copiaVetor(v1,v,tam-2);
+    copiaVetor(v2,v,tam-3);
+    cout<<"-----------------------------------"<<endl;
+    cout<<"SubVetor1"<<endl;
+    questao6AB(v1,tam-2);
+    cout<<endl;
+    cout<<"-----------------------------------"<<endl;
+    cout<<"SubVetor2"<<endl;
+    questao6AB(v2,tam-3);
+    cout<<endl;
+    cout<<"-----------------------------------"<<endl;
+}
+void questao6AB(int v[], int tam)
+{
+    printRecursive3(v,tam);
+    cout<<endl;
+    int max = questao06A(v,tam);
+    cout << "Letra (A):" << endl;
+    cout <<"MAX >> "<< max << endl;
+    cout<<endl;
+    cout << "Letra (B):" << endl;
+    questao06B(v,tam, max);
+    printRecursive3(v,tam);
+    cout<<endl;
+}
 void questao07(){}
